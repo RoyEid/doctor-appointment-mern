f16e766daec3e93d9f52b7df0a85dc796388d4e0
royeid984-code <royeid984@gmail.com>
2026-04-29 23:47:24 +0300

    Fix last img alt issue

--- frontend/src/components/HeroSlider.jsx
+++ frontend/src/components/HeroSlider.jsx
@@ -39,8 +39,11 @@ function HeroSlider() {
       <Slider {...settings}>
         {slides.map((slide, index) => (
           <div key={index} className="relative w-full h-[80vh]">
-            <img src={slide.image} className="w-full h-full object-cover" />
-
+            <img
+              src={slide.image}
+              alt={slide.title}
+              className="w-full h-full object-cover"
+            />
             <div className="absolute inset-0 bg-black bg-opacity-70 flex flex-col justify-center items-center text-center text-white px-4">
               <h2 className="text-4xl text-[#46daea] font-bold mb-4">
                 {slide.title}
